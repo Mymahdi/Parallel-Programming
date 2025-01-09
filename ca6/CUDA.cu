69c4bb63520cff595843220f214178bf1b792493
Mymahdi <henry15ronaldo79carlo1@gmail.com>
2025-01-10 02:28:27 +0330

    #5: Add image loading and memory allocation

--- ca6/CUDA.cu
+++ ca6/CUDA.cu
@@ -1,10 +1,29 @@
 #include <cuda_runtime.h>
 #include <iostream>
 #include <cmath>
+#include <opencv2/opencv.hpp>
 
-#define BLOCK_SIZE 16
+
 
 int main() {
-    std::cout << "CUDA Sobel Filter - Edge Detection" << std::endl;
+    cv::Mat image = cv::imread("images/Lenna.png", cv::IMREAD_GRAYSCALE);
+    if (image.empty()) {
+        std::cerr << "Error: Unable to load image!" << std::endl;
+        return -1;
+    }
+
+    int width = image.cols;
+    int height = image.rows;
+    size_t imgSize = width * height * sizeof(unsigned char);
+
+    unsigned char* h_input = image.data;
+    unsigned char* h_output = (unsigned char*)malloc(imgSize);
+
+    unsigned char *d_input, *d_output;
+    cudaMalloc(&d_input, imgSize);
+    cudaMalloc(&d_output, imgSize);
+    cudaMemcpy(d_input, h_input, imgSize, cudaMemcpyHostToDevice);
+
+    std::cout << "Image loaded successfully!" << std::endl;
     return 0;
 }
